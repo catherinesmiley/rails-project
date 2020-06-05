d7b15c20f135f3d8d5b5514b244a915419630416
cesmiley <catherinesmiley@gmail.com>
2020-06-05 14:37:43 -0400

    add validations to playlist and song models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,6 @@ class Playlist < ApplicationRecord
     belongs_to :user
     has_many :categories
     has_many :songs, through: :categories
+
+    validates :name, presence: true 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ApplicationRecord
     has_many :categories
     has_many :playlists, through: :categories
+
+    validates :title, presence: true 
 end

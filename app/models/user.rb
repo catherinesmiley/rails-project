5748724d36a569fd21fa8e1b5b8f235a463c66ae
cesmiley <catherinesmiley@gmail.com>
2020-06-05 14:34:34 -0400

    add has_secure_password and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :playlists
     has_many :songs, through: :playlists
+
+    has_secure_password 
+
+    validates :username, presence: true, uniqueness: true 
+    validates :password, presence: true 
 end

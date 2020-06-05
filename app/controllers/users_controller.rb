51554d54732822b0d649de3a48db15a075d6a115
cesmiley <catherinesmiley@gmail.com>
2020-06-05 14:28:30 -0400

    private strong params method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
         @user = User.new
     end 
 
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end 
+
 end

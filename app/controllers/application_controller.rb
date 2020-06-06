a883ecccc765f9d4be05541e4bc9c08b5e7ab86c
cesmiley <catherinesmiley@gmail.com>
2020-06-06 17:54:45 -0400

    created root route, home action in application controller, application folder in views, and application home view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?
 
+    def home 
+    end 
+
     def current_user
         @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#home'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   get '/login' => 'sessions#new'

8aefed8ba266a64be30230fb5c16e6d57e9d87e8
cesmiley <catherinesmiley@gmail.com>
2020-06-06 15:29:53 -0400

    added logout path to routes and destroy action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,9 @@ class SessionsController < ApplicationController
         end 
     end 
 
+    def destroy 
+        session.delete :username
+        redirect_to '/signup'
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   resources :categories
   resources :songs
   resources :playlists

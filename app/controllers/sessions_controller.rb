a110dd1ddd834d07a57580126d360e53c9f42a8c
cesmiley <catherinesmiley@gmail.com>
2020-06-08 19:38:22 -0400

    added helper method to new action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
 
     def new 
-        @user = User.new 
+        if !current_user
+            @user = User.new 
+        else 
+            redirect_to root_path
+        end 
     end 
 
     def create 

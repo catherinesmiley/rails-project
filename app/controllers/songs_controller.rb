feb1b9436a8bf9ace536019027e82041ac690f8d
cesmiley <catherinesmiley@gmail.com>
2020-06-08 19:07:33 -0400

    created index action in songs controller and songs index view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,12 @@ class SongsController < ApplicationController
         @song = Song.find_by(id: params[:id])
     end 
 
+    def index 
+        if params[:playlist_id]
+            @songs = Playlist.find(params[:playlist_id]).songs 
+        else 
+            @songs = Song.all 
+        end 
+    end 
+
 end

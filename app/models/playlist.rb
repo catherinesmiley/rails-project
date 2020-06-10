0e060367ac231dc802d6306d07020ba905b82b3c
cesmiley <catherinesmiley@gmail.com>
2020-06-10 16:48:18 -0400

    created custom songs_attributes= method in playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,7 +3,21 @@ class Playlist < ApplicationRecord
     has_many :categories
     has_many :songs, through: :categories
 
-    accepts_nested_attributes_for :songs
+    # accepts_nested_attributes_for :songs
+    # create custom songs_attributes=
+    # if song does not save, do nothing
+    # if it does, shovel into playlist (self.songs)
+
+    def songs_attributes=(song_attributes)
+        song_attributes.values.each do |song_attribute| 
+            if song_attribute[:title].present? 
+                song = Song.find_or_create_by(song_attribute)
+                if song.save
+                    self.songs << song
+                end 
+            end 
+        end 
+    end 
 
     validates :name, presence: true 
 

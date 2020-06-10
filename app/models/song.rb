cc6813241d221d5280e7c74609a390e500281565
cesmiley <catherinesmiley@gmail.com>
2020-06-10 16:11:24 -0400

    added new song link to applicatin layout

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ApplicationRecord
     has_many :categories
     has_many :playlists, through: :categories
 
-    validates :title, presence: true 
+    validates :title, presence: true
 
     scope :alphabetized, -> { order(title: :asc) }
 end

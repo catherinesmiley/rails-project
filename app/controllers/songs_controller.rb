767637ce8abb01ad50677fac5762ed8d5b408aa3
cesmiley <catherinesmiley@gmail.com>
2020-06-09 20:13:39 -0400

    fixed new action in songs controller and songs new form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,12 +11,19 @@ class SongsController < ApplicationController
     def new 
         @song = Song.new 
         playlist = Playlist.find_by(id: params[:playlist_id])
+        @song.playlists << playlist
+        # method to call @playlist.id
+        # either don't allow non-nested route for songs new or add validation here
+    end 
+
+    def create
+        byebug
     end 
 
     private 
 
     def song_params
-        params.require(:song).permit(:title, :artist, :genre, :playlist_id)
+        params.require(:song).permit(:title, :artist, :genre, playlist_ids: [])
     end 
 
 end

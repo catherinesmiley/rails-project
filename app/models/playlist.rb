040d2e26a540b3837e7da80f68d43b1128e974b0
cesmiley <catherinesmiley@gmail.com>
2020-06-18 15:25:39 -0400

    updated custom categories and songs attributes in playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,29 +5,21 @@ class Playlist < ApplicationRecord
 
     validates :name, presence: true 
 
-    accepts_nested_attributes_for :songs, :categories
+    def categories_attributes=(category_attributes)
+        category_attributes.values.each do |category_attribute|
+            if category_attribute[:name].present? && category_attribute[:song_id].present?
+                category = Category.new(category_attribute)
+                self.categories << category
+            end 
+        end 
+    end 
 
-    # def categories_attributes=(category_attributes)
-    #     category_attributes.each do |category_attribute|
-    #         if category_attribute[:name].present? 
-    #             category = Category.new(category_attribute)
-    #             category.playlist = self 
-    #             category.save
-    #         end 
-    #     end 
-    # end 
-
-    # def songs_attributes=(song_attributes)
-    #     song_attributes.values.each do |song_attribute| 
-    #         if song_attribute[:title].present?
-    #             song = Song.find_or_create_by(song_attribute)
-    #             # category = Category.find_or_create_by(song_attribute)
-    #             if song.save
-    #                 self.songs << song
-    #                 # self.categories << category 
-    #             end 
-    #         end 
-    #     end 
-    # end 
+    def songs_attributes=(song_attributes)
+        song_attributes.values.each do |song_attribute| 
+            if song_attribute[:title].present?
+                song = Song.find_or_create_by(title: song_attribute[:title]) || Song.create(song_attribute)
+            end 
+        end 
+    end 
 
 end

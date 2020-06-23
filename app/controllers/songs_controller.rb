20f70a40998471b344deec1b0722bfd23ebeb460
cesmiley <catherinesmiley@gmail.com>
2020-06-23 14:17:42 -0400

    set find_playlist as before_action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,10 @@
 class SongsController < ApplicationController
 
     before_action :logged_in?, only: [:new, :edit, :destroy]
+    before_action :find_playlist, only: [:show, :new]
 
     def show 
-        find_playlist
+        # find_playlist
         @song = Song.find_by(id: params[:id])
         if !@song 
             flash[:alert] = "That song does not exist!"
@@ -16,9 +17,9 @@ class SongsController < ApplicationController
     end 
 
     def new 
+        # find_playlist
         @song = Song.new 
         @song.categories.build
-        find_playlist
         render :new
     end 
 
@@ -32,23 +33,21 @@ class SongsController < ApplicationController
     end 
 
     def edit 
-        # if params[:playlist_id]
-        #     @playlist = Playlist.find_by(id: params[:playlist_id])
-        find_playlist
+        if params[:playlist_id]
+            @playlist = Playlist.find_by(id: params[:playlist_id])
             if @playlist.nil? || @playlist.user != current_user
                 redirect_to playlists_path 
             else 
                 @song = playlist.songs.find_by(id: params[:id])
                 redirect_to playlist_songs_path(playlist) if @song.nil?
             end
-        # else 
-
+        else 
             @song = Song.find_by(id: params[:id]) 
             if !@song
                 flash[:alert] = "This song does not exist!"
                 redirect_to songs_path
             end 
-        # end 
+        end 
     end 
 
     def update 

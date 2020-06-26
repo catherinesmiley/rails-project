25140510e0f31f3ca515e28d86f0ec3f7c9c2300
cesmiley <catherinesmiley@gmail.com>
2020-06-26 10:33:17 -0400

    added song path helper

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,11 @@
 module SongsHelper
+
+    def song_path_helper(playlist, song)
+        if playlist 
+            edit_playlist_song_path(playlist, song)
+        else 
+            edit_song_path(song)
+        end 
+    end 
+
 end
